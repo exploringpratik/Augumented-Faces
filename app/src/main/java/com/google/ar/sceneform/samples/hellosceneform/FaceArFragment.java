38977b332e2343e17925e84e9f4c620bacf50517
Your Name <you@example.com>
2020-01-03 20:55:40 +0545

    augumentd faces - codelabs

---
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/FaceArFragment.java
@@ -0,0 +1,50 @@
+package com.google.ar.sceneform.samples.hellosceneform;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.google.ar.core.Config;
+import com.google.ar.core.Config.AugmentedFaceMode;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.ux.ArFragment;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class FaceArFragment extends ArFragment {
+    /**
+     * Override to turn off planeDiscoveryController. Plane trackables are not supported with the
+     * front camera.
+     */
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FrameLayout frameLayout =
+                (FrameLayout) super.onCreateView(inflater, container, savedInstanceState);
+
+
+        getPlaneDiscoveryController().hide();
+        getPlaneDiscoveryController().setInstructionView(null);
+
+        return frameLayout;
+    }
+
+    @Override
+    protected Set<Session.Feature> getSessionFeatures() {
+        // Configure Front Camera
+        return EnumSet.of(Session.Feature.FRONT_CAMERA);
+    }
+
+    @Override
+    protected Config getSessionConfiguration(Session session) {
+        Config config = new Config(session);
+        // Configure 3D Face Mesh
+        config.setAugmentedFaceMode(AugmentedFaceMode.MESH3D);
+        return config;
+    }
+
+}
